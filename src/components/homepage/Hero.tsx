4850f18d766bca971c2099b6fc16bf9bcc5d5a32
grudov <grudovoff@iMac-grudovoff.local>
2023-05-07 22:40:50 +0800

    Update Hero.tsx

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -8,13 +8,12 @@ export const Hero: FunctionComponent = () => {
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className={clsx("hero__title", styles.title)}>
-          Hi. I'm <span className={styles.highlighted}>Paweł Kosiec</span>,
+          Привет. Я <span className={styles.highlighted}>Роман Грудов</span>,
           <br />
-          Full-stack Cloud Developer.
+          редактор LIVE GAMING
         </h1>
         <p className={clsx("hero__subtitle", styles.subtitle)}>
-          Software engineer interested in personal growth and tech trends.
-          <br /> Cloud-native and open-source enthusiast.
+          СММщик-программист, интересующийся всем подряд.
         </p>
       </div>
     </header>
